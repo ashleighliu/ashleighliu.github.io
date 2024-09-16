830a4be4723c49cf62bda619a0ce925c04cad197
Ashleigh Liu <ashleigh9302@gmail.com>
2024-09-16 16:13:29 -0400

    Fix deploy issues

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/',
   resolve: {
     alias: {
       '@components': resolve(__dirname, 'src', 'components')
